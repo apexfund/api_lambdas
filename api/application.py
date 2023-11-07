72b2d902c97cb82e23ec93b826d1dc4691047eba
Snikitha <85301357+SnikithaChelluri@users.noreply.github.com>
2023-11-06 19:27:10 -0500

    update to get ticker data from S&P 500

--- api/application.py
+++ api/application.py
@@ -3,11 +3,12 @@ import boto3
 import json
 import threading, time
 from boto3.dynamodb.conditions import Key
+import pandas as pd
 
 
 application = Flask(__name__)
 
-lambda_client = boto3.client('lambda', region_name='us-west-2')
+lambda_client = boto3.client('lambda', region_name='us-east-2')
 dynamodb = boto3.resource('dynamodb')
 historical_data_table = dynamodb.Table('HistoricalData')
 
@@ -66,7 +67,10 @@ def get_stored_data(ticker):
     return jsonify(items)
 
 def periodic_data_scrape():
-    tickers = ["AAPL", "MSFT"]
+    tickers = pd.read_html(
+    'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+    print(tickers.head())
+    tickers = tickers.Symbol.to_list()
     while True:
         for ticker in tickers:
             try:
@@ -85,13 +89,11 @@ def periodic_data_scrape():
                     )
             except Exception as e:
                 print(f"Error fetching data for {ticker}: {str(e)}")
-        # Gets data every day
-        time.sleep(86400)
+        # Gets data every hour
+        time.sleep(3600)
 
 if __name__ == "__main__":
   #  threading.Thread(target=periodic_data_scrape, daemon=True).start()
 
     application.debug = True
     application.run()
-
-
